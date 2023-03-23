72222ca840d8bd07e1dbebacdb43bd8c22c0c84f
b.m.ulyashev <b.m.ulyashev@mail.ru>
2023-03-23 22:00:09 +0300

    Правка Comment и Mapper под новый front.

--- src/main/java/ru/skypro/homework/dto/Comment.java
+++ src/main/java/ru/skypro/homework/dto/Comment.java
@@ -2,10 +2,14 @@ package ru.skypro.homework.dto;
 
 import lombok.Data;
 
+import java.time.Instant;
+
 @Data
 public class Comment {
-   private Integer author;
-    private String createdAt;
+    private Integer author;
+    private Instant createdAt;
     private Integer pk;
     private String text;
+    private String authorImage;
+    private String authorFirstName;
 }
--- src/main/java/ru/skypro/homework/mapper/CommentMapper.java
+++ src/main/java/ru/skypro/homework/mapper/CommentMapper.java
@@ -2,23 +2,36 @@ package ru.skypro.homework.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import ru.skypro.homework.dto.Comment;
 import ru.skypro.homework.model.CommentEntity;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
     @Mapping(target = "id", source = "pk")
     @Mapping(target = "user.id", source = "author")
-    @Mapping(target = "createdAt", source = "createdAt", dateFormat = "dd-MM-yyyy HH:mm:ss")
+    @Mapping(target = "user.firstName", source = "authorFirstName")
+    @Mapping(target = "createdAt", ignore = true)
     CommentEntity dtoToModel(Comment comment);
+
     @Mapping(target = "pk", source = "id")
     @Mapping(target = "author", source = "user.id")
-    @Mapping(target = "createdAt", source = "createdAt", dateFormat = "dd-MM-yyyy HH:mm:ss")
+    @Mapping(target = "authorFirstName", source = "user.firstName")
+    @Mapping(target = "createdAt", source = "createdAt", qualifiedByName = "localDateToInstant")
     Comment modelToDto(CommentEntity commentEntity);
 
     List<CommentEntity> dtoToModel(List<Comment> comments);
-    List<Comment> modelToDto (List<CommentEntity> comments);
 
+    List<Comment> modelToDto(List<CommentEntity> comments);
+
+    @Named("localDateToInstant")
+    static Instant localDateToInstant(LocalDateTime localDateTime) {
+        return ZonedDateTime.of(localDateTime, ZoneId.systemDefault()).toInstant();
+    }
 }
